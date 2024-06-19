d28e026c8ed2dd8635291e3b7779accd2ca09499
Sruthi Sivasankararaj <129876043+2149-SRUTHI-S@users.noreply.github.com>
2024-06-19 20:46:09 +0530

    Create 2. MergeSortedArray.java

---
+++ 2. MergeSortedArray.java
@@ -0,0 +1,21 @@
+/*
+Merge Two Sorted Arrays
+
+Time Complexity: O(m + n)
+Space Complexity: O(1)
+*/
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    
+        int i,j,k;
+        i = m - 1;
+        j = n - 1;
+        for(k = n + m - 1; k >= 0; k--)
+        {
+            if(i >= 0 && (j == -1|| nums1[i] > nums2[j]))
+                nums1[k] = nums1[i--];
+            else if(j >= 0)
+                nums1[k] = nums2[j--];
+        }
+    }
+}
